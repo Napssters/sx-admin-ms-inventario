b2a744e62cceae9aa28e83b92f994e259b982798
Napsster <niko.orozc@gmail.com>
2023-10-05 07:35:52 -0500

    proyecto inicial

---
+++ src/main/java/com/co/SevenXtreme/sxadminmsinventario/SxAdminMsInventarioApplication.java
@@ -0,0 +1,13 @@
+package com.co.SevenXtreme.sxadminmsinventario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SxAdminMsInventarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SxAdminMsInventarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/co/SevenXtreme/sxadminmsinventario/SxAdminMsInventarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.SevenXtreme.sxadminmsinventario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SxAdminMsInventarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
